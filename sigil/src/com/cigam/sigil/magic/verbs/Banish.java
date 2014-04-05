ecfccb7a0798a0abfdb2705550ef1bc35799243b
Eli Davis <ebdavis@mit.edu>
2014-04-05 19:30:55 -0400

    Banish runes actually created and added

---
+++ sigil/src/com/cigam/sigil/magic/verbs/Banish.java
@@ -0,0 +1,55 @@
+package com.cigam.sigil.magic.verbs;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.cigam.sigil.PhysicalEntity;
+import com.cigam.sigil.magic.*;
+import com.cigam.sigil.materials.Summoning;
+import com.cigam.sigil.screens.*;
+
+
+public class Banish extends Verb {
+	private SpellDescriptor toSummonTo;
+	private ArrayList<SpellDescriptor> summonCriteria;
+	private float defaultDuration = 10;
+	private float defaultRadius = 300;
+	
+	public Banish(Verb target, ArrayList<Spell> args) {
+		super(target, args);
+		summonCriteria = new ArrayList<SpellDescriptor>();
+		area = new CircleShape();
+		area.setRadius(defaultRadius);
+	}
+	public Banish(PhysicalEntity c, AdventureScreen b, Target target, ArrayList<Spell> args) {
+		super(c, b, target, args);
+		summonCriteria = new ArrayList<SpellDescriptor>();
+		area = new CircleShape();
+		area.setRadius(defaultRadius);
+	}
+	
+	@Override
+	public void topEvalEffect(){
+		toSummonTo = target.evalEffect();
+		for(Spell s: arguments){
+			summonCriteria.add(s.evalEffect());
+		}
+	}
+
+	@Override
+	public SpellDescriptor evalEffect() {
+		toSummonTo = target.evalEffect();
+		for(Spell s: arguments){
+			summonCriteria.add(s.evalEffect());
+		}
+		System.out.println(caster);
+		SpellDescriptor effect = new SpellDescriptor(new Summoning(toSummonTo, summonCriteria), defaultDuration, toSummonTo, summonCriteria, caster.body.getAngle(), area, caster.body.getPosition());
+		return effect;
+	}
+	@Override
+	public void cast() {
+	//System.out.println(target.bd.position + " is caster location");
+	//System.out.println(bd.position.add(castDir.mul(this.fd.shape.m_radius)) + " is created object location");
+	screen.createSpellEffect(evalEffect());
+	}
+}
