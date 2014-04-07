115f8428c38c5f399a736d5001373819e40cc6d3
Jayson Lynch <leibniz136@gmail.com>
2014-04-07 16:49:21 -0400

    Basic UI stuff seems to be working, won't really have more time before Friday. I have no love for this system.

--- sigil/src/com/cigam/sigil/screens/PauseScreen.java
+++ sigil/src/com/cigam/sigil/screens/PauseScreen.java
@@ -5,24 +5,104 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Pixmap.Format;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
 import com.cigam.sigil.SigilGame;
 
 public class PauseScreen implements Screen {
 	SigilGame game;
 	Screen parent;
 	Sprite pauseImage;
+	Stage stage;
+	Skin skin;
+	String spellString;
+	TextField spellInput;
 
-	public PauseScreen(SigilGame game, Screen parent) {
+	public PauseScreen(final SigilGame game, final Screen parent) {
 		this.game = game;
 		this.parent = parent;
 		Texture texture = new Texture(Gdx.files.internal("art/screens/pause.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+
+		
+		
+		
+		Skin skin = new Skin();
+		
+		// Generate a 1x1 white texture and store it in the skin named "white".
+		Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+		pixmap.setColor(Color.WHITE);
+		pixmap.fill();
+		skin.add("white", new Texture(pixmap));
+
+		// Store the default libgdx font under the name "default".
+		skin.add("default", new BitmapFont());
+
+		// Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+		textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+		textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+		textButtonStyle.font = skin.getFont("default");
+		skin.add("default", textButtonStyle);
+		
+		// Trying to make a textfield
+		TextFieldStyle textFieldStyle = new TextFieldStyle();
+		textFieldStyle.font = skin.getFont("default");
+		textFieldStyle.fontColor = Color.BLUE;
+		skin.add("default", textFieldStyle);
+		spellInput = new TextField("Enter Spell", skin);
+
+		// Create a table that fills the screen. Everything else will go inside this table.
+		Table table = new Table();
+		table.setFillParent(true);
+		stage.addActor(table);
+		//table.setSize(260, 195);
+		table.setPosition(0, 0);
+		//table.align(Align.right | Align.bottom);
+
+		// Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+		final TextButton resume = new TextButton("Resume Game", skin);
+		table.add(resume);
+		table.row();
+		table.add(spellInput);
+		
+		// Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+		// Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+		// ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+		// revert the checked state.
+		resume.addListener(new ChangeListener() {
+			public void changed (ChangeEvent event, Actor actor) {
+					//System.out.println("Clicked! Is checked: " + resume.isChecked());
+					game.setScreen(parent);
+			}
+		});
+		
+		
+		
+		
 		pauseImage = new Sprite(texture);
 	}
 
@@ -43,7 +123,18 @@ public class PauseScreen implements Screen {
 		pauseImage.draw(game.hudBatch);
 		game.hudBatch.end();
 		
-		if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
+		stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+		stage.draw();
+		Table.drawDebug(stage);
+
+		if(Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+			spellString = spellInput.getMessageText();
+			System.out.println(spellString);
+			spellInput.setText("Enter Spell");
+		}
+		
+		
+		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
 			game.setScreen(parent);
 	}
 
@@ -80,6 +171,7 @@ public class PauseScreen implements Screen {
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		stage.dispose();
+		skin.dispose();
 	}
 }
